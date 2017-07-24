4ffac9a1a07ba1918f362e153f61410275321871
Chen Feng <puck.chen@foxmail.com>
2017-07-24 22:52:23 +0800

    fix match 2 arrary
    Signed-off-by: Chen Feng <puck.chen@foxmail.com>

--- match_2_arrary.c
+++ match_2_arrary.c
@@ -8,23 +8,23 @@ int match(int *arrary, int len, int max)
 	int i = 0;
 	int j = len - 1;
 	while (i < j) {
-		if (arrary[j] == max) {
-			num++;
-			j--;
-		}
 		if (arrary[i] + arrary[j] <= max) {
 			num++;
 			i++;
 			j--;
 		} else {
+			if (arrary[j] <= max)
+				num ++;
 			j--;
 		}
 	}
+	if (i == j && arrary[i] <= max)
+		num ++;
 	return num;
 }
 
 int main()
 {
-	int a[] = {20, 20, 30, 50, 60, 70, 80, 90, 90, 100};
-	printf("%d\n", match(a, 10, 100));
+	int a[] = {20, 20, 30, 50, 60, 70, 80, 90, 90, 90, 100, 101};
+	printf("%d\n", match(a, 11, 100));
 }
